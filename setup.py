ff072109e61bbce88ee6e01149cb127152c87e2d
Samuel Chai <samuel.chai.development@gmail.com>
2023-05-07 18:02:10 +0300

    changing versions

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='impose-cli',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple package that creates a CLI tool using just a few decorators.',
     author='Samuel Chai',
     author_email='samuel.chai.development@gmail.com',
     packages=find_packages(),
-    install_requires=[line.strip() for line in open('requirements.txt', 'r').readlines()]
+    install_requires=["click"]
 )
