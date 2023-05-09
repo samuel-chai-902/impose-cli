96ca9a96dbe3a1645bcec2b68121e1a6038b2a00
Samuel Chai <samuel.chai.development@gmail.com>
2023-05-09 09:48:27 +0300

    fixing version

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
-from subprocess import run, CalledProcessError
+from subprocess import run
+from os import environ
 from setuptools import setup, find_packages
-
-
-def get_version():
-    output = run('git describe --tags', capture_output=True, shell=True).stdout.decode('utf-8')
-    return ''.join(filter(lambda x: x.isdigit() or x == '.', list(output)))
 
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-VERSION = get_version()
 DESCRIPTION = 'Create a CLI tool easily.'
 
 # Setting up
 setup(
     name="impose-cli",
-    version=VERSION,
+    version=environ['BRANCH_TAG'],
     author="scdev",
     author_email="samuel.chai.development@gmail.com",
     description=DESCRIPTION,
