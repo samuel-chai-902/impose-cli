541d5dfa7451a75ca979bb03a4daa7e4549f6bcd
Samuel Chai <samuel.chai.development@gmail.com>
2023-07-09 11:19:19 +0300

    adding release canditate change

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = 'Create a CLI tool easily.'
 # Setting up
 setup(
     name="impose-cli",
-    version='0.1.41rc7',
+    version='0.1.41rc8',
     author="scdev",
     author_email="samuel.chai.development@gmail.com",
     description=DESCRIPTION,
